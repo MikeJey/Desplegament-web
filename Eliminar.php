801f6dc962aff3bd97a4d10084d092bd645fc6a3
MikeJey <majuanmartinez@gmail.com>
2024-04-29 22:43:53 +0200

    Añado los comentarios al archivo Eliminar.php. v2

--- Eliminar.php
+++ Eliminar.php
@@ -3,10 +3,24 @@
     require_once('Connexio.php');
     require_once('Header.php');
     
+    /**
+     * Clase que se encarga de gestionar la eliminación de productos.
+     */
     class Eliminar {
         
+        /**
+         * Función que elimina el producto cuya $id se inserta por parámetro.
+         * 
+         * @param type $id
+         * @return type
+         */
         public function eliminaProducte($id) {
             
+            /**
+             * Si no se recibe una $id, o si ésta no es numérica, se lanzará un
+             * mensaje avisando de que el ID del producto no es válido y se 
+             * saldrá de la aplicación.
+             */
             if (!isset($id) || !is_numeric($id)) {
                 echo '<p>ID de producto no válido.</p>';
                 return;
@@ -23,6 +37,11 @@
                     <hr>
                     <p>';
             
+            /**
+             * Si el $resultado es correcto, se avisará al usuario de que el 
+             * producto se ha eliminado correctamente. Por el contrario, se
+             * notificará que ha havbido un error.
+             */
             if ($resultado === TRUE) {
                 echo 'Producto eliminado correctamente.';
             } else {
